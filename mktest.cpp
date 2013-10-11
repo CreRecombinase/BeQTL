eb83a119fe8c8a356aff1048d19bf11a7e0d7bbf
CreRecombinase <nwknoblauch@gmail.com>
2013-10-11 15:38:53 -0400

    changed code to facilitate Job Arrays

--- mktest.cpp
+++ mktest.cpp
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include "mkl.h"
 #include "mktest.hpp"
+#include "readwritemat.hpp"
 #include "hdf5.h"
 #include "hdf5_hl.h"
-#include <sys/file.h>
+#include <sstream>
+#include "strideiter.hpp"
 
 
 
@@ -14,9 +16,9 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-  if(argc<6)
+  if(argc<5)
     {
-      cerr<<"usage: "<<argv[0]<<" startchunk endchunk snpgenefilename readfilelock writefilelock progfile"<<endl;
+      cerr<<"usage: "<<argv[0]<<" startchunk numchunks snpgenefilename threads progbar"<<endl;
       return(-1);
     }
   
@@ -27,32 +29,39 @@ int main(int argc, char* argv[])
  
   //Matrices and temporary vectors for reporting and storing quantiles
   double *tquantile;
+  int* snpannoarray,*geneannoarray;
+
   
 
   //file parameters
-
-  char *snpgenefilename,*annofilename,*cisfilename,*transfilename,*readfilelock,*writefilelock,*progfile;
+  char *snpgenefilename,*annofilename,*tcisfilename,*ttransfilename,*tprogfilename;
+  
 
 
   // Platform data/case numbers
-  int casetotal;
-  int snptotal;
-  int genetotal;
+  int progbar;
+  int threads;
+  int snptotal,genetotal;
+  int snpabstotal,geneabstotal;
 
   //variables for profiling
   double s_initial_boot,s_elapsed_boot;
   double s_initial_mult, s_elapsed_mult;
   double s_initial_quant,s_elapsed_quant;
+  double s_initial_quant2,s_elapsed_quant2;
   double s_initial_write,s_elapsed_write;
   double s_initial_memset,s_elapsed_memset;
   double s_initial_read,s_elapsed_read;
 
   int mychunk,lastchunk,mysnpchunk,mygenechunk,chunkstart;
+  int numchunks;
   int snpchunks;
   int genechunks;
 
-  int snpsize;
-  int genesize;
+  int osnpsize;
+  int ogenesize;
+  size_t snpsize;
+  size_t genesize;
 
   int casesize;
   int snpstart;
@@ -61,38 +70,51 @@ int main(int argc, char* argv[])
   int cisdist;
   int bsi;
   int alignsize=64;
+  int quantilechunks;
   
-  double alpha,beta,t_thresh,mean=0;
+  double alpha,beta,t_thresh;
   int  *bootrows;
   size_t C_dim=0,new_c_dim=0;
   
   chunkstart = atoi(argv[1]);
-  lastchunk = atoi(argv[2]);
+  numchunks = atoi(argv[2]);
+  lastchunk=chunkstart+numchunks;
   snpgenefilename=argv[3];
-  readfilelock=argv[4];
-  writefilelock=argv[5];
-  progfile=argv[6];
+  threads=atoi(argv[4]);
+  threads=threads*2;
+  progbar=atoi(argv[5]);
   
   hid_t file_id;
+  herr_t err=0;
   file_id = H5Fopen(snpgenefilename,H5F_ACC_RDONLY,H5P_DEFAULT);
 
-  readparam(file_id,"annofile",annofilename);
-  readparam(file_id,"cisfilename",cisfilename);
-  readparam(file_id,"transfilename",transfilename);
-  readparam(file_id,"snpchunks",snpchunks);
-  readparam(file_id,"genechunks",genechunks);
-  readparam(file_id,"snptotal",snptotal);
-  readparam(file_id,"genetotal",genetotal);
-  readparam(file_id,"casetotal",casesize);
-  readparam(file_id,"snpsize",snpsize);
-  readparam(file_id,"genesize",genesize);
-  casetotal=casesize;
-  readparam(file_id,"bsi",bsi);
+  err=err|readparam(file_id,"annofile",annofilename);
+  err=err|readparam(file_id,"cisfilename",tcisfilename);
 
-  readparam(file_id,"t_thresh",t_thresh);
-  readparam(file_id,"cisdist",cisdist);
+  err=err|readparam(file_id,"transfilename",ttransfilename);
+  err=err|readparam(file_id,"progfile",tprogfilename);
+  err=err|readparam(file_id,"snpchunks",snpchunks);
+  err=err|readparam(file_id,"genechunks",genechunks);
+  err=err|readparam(file_id,"snptotal",snptotal);
+  err=err|readparam(file_id,"genetotal",genetotal);
+  err=err|readparam(file_id,"casetotal",casesize);
+  err=err|readparam(file_id,"snpsize",osnpsize);
+  snpsize=osnpsize;
+  err=err|readparam(file_id,"genesize",ogenesize);
+  genesize=ogenesize;
+  err=err|readparam(file_id,"bsi",bsi);
+  err=err|readparam(file_id,"snpabstotal",snpabstotal);
+  err=err|readparam(file_id,"geneabstotal",geneabstotal);
+
+  err=err|readparam(file_id,"t_thresh",t_thresh);
+  err=err|readparam(file_id,"cisdist",cisdist);
+  if(err<0){
+    cerr<<"Parameter not successfully read!: "<<err<<endl;
+    return(err);
+  }
 
   H5Fclose(file_id);
+
 
 
   cout<<"parameterization successful!"<<endl;
@@ -100,15 +122,54 @@ int main(int argc, char* argv[])
 
 
   alpha = 1.0/((double)casesize);
+
   beta = 0.0;  
   s_elapsed_read=0;
   s_elapsed_boot=0;
   s_elapsed_mult=0;
   s_elapsed_quant=0;
   s_elapsed_write=0;
+
+  //Set number of threads to be number of slots on LSF
+  mkl_set_num_threads(threads);
+
+  cout<<"Initializing correlation array of size: "<<snpsize<<"*"<<genesize<<"*"<<bsi<<"*"<<sizeof(double)<<"="<<new_c_dim<<endl; 
+  C_dim = (size_t)snpsize*(size_t)genesize*(size_t)bsi*sizeof(double);
+  C = (double *)mkl_malloc( C_dim,alignsize);
+  Snpmat = (double*)mkl_malloc(casesize*snpsize*sizeof(double),alignsize);
+  Genemat = (double*)mkl_malloc(casesize*snpsize*sizeof(double),alignsize);
+  BootSNP= (double *) mkl_malloc(casesize*snpsize*sizeof(double),alignsize);
+  BootGene= (double *) mkl_malloc(casesize*genesize*sizeof(double),alignsize);  
+  tquantile = (double*)mkl_malloc(3*(size_t)snpsize*(size_t)genesize*sizeof(double),alignsize);
+  bootrows = (int*) mkl_malloc((size_t)casesize*(size_t)bsi*sizeof(int),alignsize);
+  snpannoarray= (int*) mkl_malloc(snpabstotal*2*sizeof(int),alignsize);
+  geneannoarray= (int*) mkl_malloc(geneabstotal*3*sizeof(int),alignsize);
+
+
+  ReadMatrix(snpannoarray,0,snpabstotal,0,2,annofilename,"snparray");
+  ReadMatrix(geneannoarray,0,geneabstotal,0,3,annofilename,"genearray");
+
   
+  map<char*,int,cmp_str> snpanno=ReadAnno(annofilename,2,"snpname");
+  map<char*,int,cmp_str> geneanno=ReadAnno(annofilename,3,"genename");
+
+
+  if(C==NULL)
+    {
+      
+      cout<<"Not enough memory for corarray!"<<endl;
+      return(-1);
+    }
+  C[(C_dim/sizeof(double))-1]=0;
   for(mychunk =chunkstart; mychunk<=lastchunk; mychunk++)
     {
+      stringstream sss;
+      sss<<mychunk;
+      string chunkstr=sss.str();
+      string progfilename=string(tprogfilename)+std::string(chunkstr)+".txt";
+      string cisfilename=string(tcisfilename)+std::string(chunkstr)+".txt";
+      string transfilename=string(ttransfilename)+std::string(chunkstr)+".txt";
+     
       if(mychunk>snpchunks*genechunks)
 	{
 	  cout<<"eQTL analysis complete!"<<endl;
@@ -118,7 +179,8 @@ int main(int argc, char* argv[])
       //parameters set from mychunk
       mysnpchunk  = mychunk/(genechunks);
       mygenechunk = mychunk%(genechunks);
-
+      snpsize=osnpsize;
+      genesize=ogenesize;
       snpstart =mysnpchunk*snpsize;
       if(snpsize+snpstart>snptotal)
 	{
@@ -131,31 +193,6 @@ int main(int argc, char* argv[])
 	}
       cout<<"iteration number: "<<mychunk<<" SNPchunk: "<<mysnpchunk<<" Genechunk: "<<mygenechunk<<endl;
       //Size of result array
-      new_c_dim = (size_t)snpsize*(size_t)genesize*(size_t)bsi*sizeof(double);
-
-
-  
-      cout<<"Initializing large corarray of size:"<<snpsize<<"*"<<genesize<<"*"<<bsi<<"*"<<sizeof(double)<<"="<<new_c_dim<<endl;
-      if(new_c_dim>C_dim)
-	{
-	  if(C!=NULL)
-	    {
-	      mkl_free(C);
-	      mkl_free(BootSNP);
-	      mkl_free(BootGene);
-	    }
-	  C_dim = (size_t)snpsize*(size_t)genesize*(size_t)bsi*sizeof(double);
-	  C = (double *)mkl_malloc( C_dim,alignsize);
-	  BootSNP= (double *) mkl_malloc(casesize*snpsize*sizeof(double),alignsize);
-	  BootGene= (double *) mkl_malloc(casesize*genesize*sizeof(double),alignsize);
-	}
-
-      if(C==NULL)
-	{
-
-	  cout<<"Bad things happened!"<<endl;
-	}
-      C[(C_dim/sizeof(double))-1]=0;
 
     
       cout<<"SNPrange= "<<snpstart<<"-"<<snpstart+snpsize<<endl;
@@ -163,32 +200,31 @@ int main(int argc, char* argv[])
       
       s_initial_read=dsecnd();
       cout<<"Reading in gene matrix!"<<endl;
-      ReadMatrix(Genemat,casetotal,genetotal,casestart,casesize,genestart,genesize,snpgenefilename,"genes",readfilelock);
-      cblas_daxpby(casesize*genesize,100,Genemat,1,0,Genemat,1);
+      ReadMatrix(Genemat,casestart,casesize,genestart,genesize,snpgenefilename,"genes");
+      cblas_dscal(casesize*genesize,100,Genemat,1);
 	  
       cout<<"Reading in SNP matrix!"<<endl;
-      ReadMatrix(Snpmat,casetotal,snptotal,casestart,casesize,snpstart,snpsize,snpgenefilename,"snps",readfilelock);
-   
-  
+      ReadMatrix(Snpmat,casestart,casesize,snpstart,snpsize,snpgenefilename,"snps");
       s_elapsed_read += dsecnd()-s_initial_read;
       
       //Bootstrapping loop
       //Initialize Bootstrap Matrices
      
       
-      cout<<"Making bootstrap"<<endl;
       if(mychunk==chunkstart)
 	{
 	  MakeBootRows(bootrows,casesize,bsi);
 	}
 
-      alpha = 1/((double) casesize-1);
+
 
       cout<<"Starting Bootstrap"<<endl;
-      //  s_elapsed_write=0;
       for( size_t q=0; q<bsi; q++){
 	int percent = (((double) q)/((double) bsi))*100;
-	printProgBar(percent);
+	if(progbar>0)
+	  {
+	    printProgBar(percent);
+	  }
 	//	cout<<q<<endl;
 	s_initial_boot=dsecnd();
 	DoBootstrap(BootSNP,Snpmat,casesize,snpsize,&bootrows[index(q,0,casesize)]);
@@ -200,53 +236,52 @@ int main(int argc, char* argv[])
 	size_t ci = snpsize*genesize*q;
 	//	cout<<ci<<"\t"<<C_dim<<endl;
 	
-	//	cout<<"scaling done; about to mult"<<endl;
+
 	s_initial_mult=dsecnd();
-	
 	cblas_dgemm(CblasRowMajor, CblasTrans, CblasNoTrans, snpsize, genesize, casesize,1, BootSNP, snpsize, BootGene, genesize, beta, &C[ci], genesize);
 	s_elapsed_mult+=(dsecnd()-s_initial_mult);
 	
-
-	//PrintMat(&C[(size_t)(snpsize*genesize*q)],snpsize,genesize);
-	//	PrintMat(C,snpsize,genesize);
       }
       
-      //            cout<<"Deallocating memory"<<endl;
-      
-      
-      mkl_free(Snpmat);
-      mkl_free(Genemat);
-      if(mychunk==lastchunk)
-	{
-	  mkl_free(BootSNP);
-	  mkl_free(BootGene);
-	}
+
  
       //Initializing quantile matrix
       //    cout<<"Allocating quantile matrix"<<endl;
-      tquantile = (double*)mkl_malloc(2*(size_t)snpsize*(size_t)genesize*sizeof(double),alignsize);
 
-      //    cout<<"Computing quantiles"<<endl;
+
+      cout<<"Computing quantiles"<<endl;
+
+
       s_initial_quant=dsecnd();
       GetQuantile(C,snpsize,genesize,bsi,tquantile);
+      //cout<<"Writing quantiles to "<<corfilename<<endl;
+      //WriteQuantiles(corfilename,tquantile,snpstart,snpsize,genestart,genesize,mychunk,writefilelock,snpgenefilename);
       s_elapsed_quant+=dsecnd()-s_initial_quant;
-
-      if(mychunk==lastchunk)
-	{
-	  mkl_free(C);
-	}
-
       cout<<"Writing quantiles to file: "<<cisfilename<<endl;
       s_initial_write=dsecnd();
-      CisTransOut(tquantile,snpstart,snpsize,genestart,genesize,casesize,2,annofilename,t_thresh,cisfilename,transfilename,cisdist,writefilelock,mychunk,progfile);
+      CisTransOut(tquantile,snpstart,snpsize,genestart,genesize,casesize,3,annofilename,t_thresh,cisfilename.c_str(),transfilename.c_str(),cisdist,mychunk,progfilename.c_str(),snpanno,geneanno,snpannoarray,geneannoarray,snpgenefilename);
       s_elapsed_write+=dsecnd()-s_initial_write;
-      mkl_free(tquantile);
       cout<<"Read time: "<<s_elapsed_read<<endl;
       cout<<"Bootstrap time: "<<s_elapsed_boot<<endl;
       cout<<"Mult time: "<<s_elapsed_mult<<endl;
       cout<<"Quantile time: "<<s_elapsed_quant<<endl;
       cout<<"Write time: "<<s_elapsed_write<<endl;
+
+
+      s_elapsed_read=0;
+      s_elapsed_boot=0;
+      s_elapsed_mult=0;
+      s_elapsed_quant=0;
+      s_elapsed_write=0;
     }
+      mkl_free(Snpmat);
+      mkl_free(bootrows);
+      mkl_free(tquantile);
+      mkl_free(Genemat);
+      mkl_free(C);
+      mkl_free(BootSNP);
+      mkl_free(BootGene);
+
 
   return (0);
 }
--- mktest.hpp
+++ mktest.hpp
@@ -1,22 +1,43 @@
 #ifndef MKTEST_HPP
 #define MKTEST_HPP
-#include <string>
-#include <fstream>
-#include "hdf5.h"
 #include <math.h>
 #include "mkl.h"
-#include "hdf5_hl.h"
-#include <sys/file.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "strideiter.hpp"
+
+#include <algorithm>
 
 
 
 bool debug=false;
 using namespace std;
-enum snp_exp {SNP,GENE};
+
+
+
+void printProgBar(int percent){
+  cout<<"[";
+  for(int i=0; i<50; i++)
+    {
+      if(i<(percent/2))
+	{
+	  cout<<"=";
+
+	}
+      else 
+	{
+	  if (i==(percent/2))
+	    {
+	      cout<<">";
+
+	    }
+	  else
+	    {
+	      cout<<" ";
+	    }
+	}
+    }
+  cout<<"]"<<percent<<" %\r";
+  cout.flush();
+}
 
 size_t multindex(size_t row,size_t column,size_t zindex,size_t nrows,size_t ncols,size_t zsize)
 {
@@ -33,55 +54,7 @@ size_t index (size_t row, size_t column, size_t nocols)
   return((nocols*row)+column);
 }
 
-void PrintMat(const double *matrix,int r,int c, const char* filename)
-{
-  ofstream file;
-  file.open(filename,ofstream::out);
-  for(int i=0;i<r;i++)
-    {
-      for(int j=0;j<c;j++)
-	{
-	  file<<matrix[index(i,j,c)]<<"\t";
-	}
-      file<<"\n";
-    }
-  file.close();
-  //    cout<<"____________________"<<endl;
-}
 
-void PrintMat(const double *matrix,int r,int c)
-//Function for printing 2D, row order array
-//*matrix   pointer to head of matrix
-//r         number of rows
-//c         number of columns
-{
-  for(int i=0;i<r;i++)
-    {
-      for(int j=0;j<c;j++)
-	{
-	  cout<<matrix[index(i,j,c)]<<"\t";
-	}
-      cout<<"\n";
-    }
-    cout<<"____________________"<<endl;
-}
-
-void PrintMat(const int *matrix,int r,int c)
-//Function for printing 2D, row order array
-//*matrix   pointer to head of matrix
-//r         number of rows
-//c         number of columns
-{
-  for(int i=0;i<r;i++)
-    {
-      for(int j=0;j<c;j++)
-	{
-	  cout<<matrix[index(i,j,c)]<<"\t";
-	}
-      cout<<"\n";
-    }
-    cout<<"____________________"<<endl;
-}
 
 
 void DoScale(double *&matrix,int rows,int cols)
@@ -131,7 +104,7 @@ void MakeBootRows(int *&bootrows, int rowsize,int bstotal)
   VSLStreamStatePtr stream;
   int errcode;
   
-  bootrows = (int*) mkl_malloc((size_t)rowsize*(size_t)bstotal*sizeof(int),64);
+
   errcode = vslNewStream(&stream,VSL_BRNG_MCG31,123);
   errcode = viRngUniform(VSL_RNG_METHOD_UNIFORM_STD,stream,rowsize*bstotal,bootrows,0,rowsize);
   errcode = vslDeleteStream(&stream);
@@ -170,219 +143,78 @@ void DoBootstrap(double *&bootmatrix,const double* omatrix,int rowsize, int cols
    
 }
 
-void printProgBar(int percent){
-  char bar[50];
-  
-  for(int i=0; i<50; i++)
-    {
-      if(i<(percent/2))
-	{
-	  bar[i]='=';
-
-	}
-      else 
-	{
-	  if (i==(percent/2))
-	    {
-	      bar[i]='>';
-
-	    }
-	  else
-	    {
-	      bar[i]=' ';
-	    }
-	}
-    }
-  cout<<"\r" "["<<bar<<"] ";
-  cout.width(3);
-  cout<<percent<<"%     "<<flush;
-}
-
-void MatSlice(double *&slicemat, double *&omat,int colstart,int colsize,int rowsize, int zsize)
-//return 2d slices of a 3d array
-{
-  for(int i=0; i<zsize; i++)
-    {
-
-      cblas_dcopy(rowsize,&omat[multindex(0,colstart,i,rowsize,colsize,zsize)],colsize,&slicemat[index(0,i,zsize)],zsize);
-    }
-}
-
-
-herr_t GetSlice(int rowstart,int colstart,int bsistart,int rownum, int colnum,int bsinum,double *&matrix,hid_t file, hid_t dataset,hid_t dataspace)
-{
-
-  //This in theory is the dimension of the ouput data (All snps, one gene, all bootstrap iterations)
-  hsize_t slabsize[3]={rownum,colnum,bsinum};
-
-  hsize_t offset[3]={rowstart,colstart,bsistart};
-
-  hsize_t offset_out[3]={0,0,0};
-  hid_t memspace;
-  herr_t status;
-
-
-  status = H5Sselect_hyperslab(dataspace,H5S_SELECT_SET,offset,NULL,slabsize,NULL);
-  
-  memspace = H5Screate_simple(3,slabsize,NULL);
-
-  status = H5Sselect_hyperslab(memspace,H5S_SELECT_SET,offset_out,NULL,slabsize,NULL);
-  status = H5Dread(dataset,H5T_NATIVE_DOUBLE,memspace,dataspace,H5P_DEFAULT,matrix);
-
-   H5Sclose(memspace);
-   return(status);
-}
-
-
-herr_t ReadMatrix(double *&matrix,int rtot,int ctot,int rstart,int rsize, int cstart, int csize,const char* filename,const char* snpgene,const char*lockfile)
-{
-  struct stat buffer;
-  hid_t dataspace,file,dataset,rank,memspace;
-  herr_t status;
-  FILE * fp;
-  hsize_t dimsout[2]={rsize,csize};
-  hsize_t offset[2]={rstart,cstart};
-  hsize_t offset_out[2] ={0,0};
-  hsize_t dimsm[2] ={rsize,csize};
-
-   
-  
-  matrix = (double *)mkl_malloc(rsize*csize*sizeof(double),64);
-  
-  //  cout<<"obtaining file lock"<<endl;
-  while(stat(lockfile,&buffer)==0)
-    {
-      usleep(999999);
-      cout<<lockfile<<" exists!"<<endl;
-    }
-  fp = fopen(lockfile,"ab+");
-  
-  //  cout<<"file lock obtained"<<endl;
-
-  file  = H5Fopen(filename,H5F_ACC_RDONLY,H5P_DEFAULT);
-  dataset = H5Dopen2(file,snpgene,H5P_DEFAULT);
-  dataspace = H5Dget_space(dataset);
-
-  status = H5Sselect_hyperslab(dataspace,H5S_SELECT_SET,offset,NULL,dimsm,NULL);
-
-  memspace = H5Screate_simple(2,dimsm,NULL);
-
-  status = H5Sselect_hyperslab(memspace,H5S_SELECT_SET,offset_out,NULL,dimsm,NULL);
-  status = H5Dread(dataset,H5T_NATIVE_DOUBLE,memspace,dataspace,H5P_DEFAULT,matrix);
-  
-  status = H5Dclose(dataset);
-  status = H5Sclose(dataspace);
-  status = H5Fclose(file);
-
-  fclose(fp);
-  remove(lockfile);
-
-  return(status);
-
-
-
-
-}
-
-void ReadAnno (char **&name, int *&array, int numstart,int numsize,const char* filename,int arraydim)
-{
-  
-  hid_t dataspacename,dataspacearray,file_id,datasetname,datasetarray,rank,memspacename,memspacearray;
-  hid_t fstrtype;
-  hid_t mstrtype;
-  hsize_t offsetname[1]={numstart};
-  hsize_t offsetarray[2]={numstart,0};
-  hsize_t dimsarray[2]={numsize,arraydim};
-  hsize_t dimsname[1]={numsize};
-  hsize_t offset_outname[1]={0};
-  hsize_t offset_outarray[2]={0,0};
-  herr_t status;
-  
-  
-  array = (int *)mkl_malloc(arraydim*numsize*sizeof(int),64);
-  name = (char**)mkl_malloc(numsize*sizeof(char*),64);
-  
-  file_id = H5Fopen(filename,H5F_ACC_RDONLY,H5P_DEFAULT);
-  if(arraydim==2)
-    {
-      datasetname = H5Dopen2(file_id,"snpname",H5P_DEFAULT);
-      datasetarray = H5Dopen2(file_id,"snparray",H5P_DEFAULT);
-      
-    }
-  else
-    {
-      datasetname = H5Dopen2(file_id,"genename",H5P_DEFAULT);
-      datasetarray = H5Dopen2(file_id,"genearray",H5P_DEFAULT);
-    }
-  dataspacename = H5Dget_space(datasetname);
-  dataspacearray = H5Dget_space(datasetarray);
-  
-  status = H5Sselect_hyperslab(dataspacename,H5S_SELECT_SET,offsetname,NULL,dimsname,NULL);
-  status = H5Sselect_hyperslab(dataspacearray,H5S_SELECT_SET,offsetarray,NULL,dimsarray,NULL);
-
-  memspacename=H5Screate_simple(1,dimsname,NULL);
-  memspacearray=H5Screate_simple(2,dimsarray,NULL);
-  
-
-  fstrtype = H5Tcopy(H5T_C_S1);
-  status = H5Tset_size(fstrtype,H5T_VARIABLE);
-  mstrtype = H5Tcopy(H5T_C_S1);
-  status= H5Tset_size(mstrtype,H5T_VARIABLE);
-  
-  status = H5Sselect_hyperslab(memspacename,H5S_SELECT_SET,offset_outname,NULL,dimsname,NULL);
-  status= H5Sselect_hyperslab(memspacearray,H5S_SELECT_SET,offset_outarray,NULL,dimsarray,NULL);
-
-  status = H5Dread(datasetname,fstrtype,memspacename,dataspacename,H5P_DEFAULT,name);
-  status = H5Dread(datasetarray,H5T_NATIVE_INT,memspacearray,dataspacearray,H5P_DEFAULT,array);
-
-  status = H5Tclose(fstrtype);
-  status = H5Tclose(mstrtype);
-  status = H5Dclose(datasetname);
-  status = H5Dclose(datasetarray);
-  status = H5Sclose(dataspacename);
-  status = H5Sclose(dataspacearray);
-  status = H5Fclose(file_id);
-  
-
-}
 
 
 
 void GetQuantile(double *&matrix, size_t rowsize,size_t colsize, size_t bootsize,double *&quantiles)
+//Let's try to do the quantile estimation in four chunks if we can't get quantile estimation to work off the bat
 {
 
-  int deciles = 2;
+  int qsize=1;
+  int deciles = 3;
   VSLSSTaskPtr task;
   int status;
   double variation[colsize*rowsize];
   double mean[colsize*rowsize];
   double r2[colsize*rowsize];
-  double o_quant[2]={0.025,0.975};
+  double o_quant[3]={0.025,0.5,0.975};
 
-  double o_stat[colsize*rowsize*bootsize];
+  double* o_stat=NULL;
+  double*tmatrix = NULL;
   MKL_INT x_storage =VSL_SS_MATRIX_STORAGE_COLS;
   MKL_INT o_storage = VSL_SS_MATRIX_STORAGE_ROWS;
   unsigned MKL_INT64 estimate=0;
   int p = rowsize*colsize;
   int n=bootsize;
-  
 
-  status = vsldSSNewTask( &task,&p,&n,&x_storage,(double*)matrix,NULL,NULL);
+  o_stat = (double*)mkl_malloc(colsize*rowsize*bootsize*sizeof(double),64);
+  if(o_stat==NULL||debug)
+    {
+      qsize=4;
+      p=(rowsize/qsize)*colsize;
+      if(o_stat==NULL){
+	o_stat=(double*)mkl_malloc(colsize*(rowsize/qsize)*bootsize*sizeof(double),64);
+      }
+      tmatrix=(double*)mkl_malloc(colsize*(rowsize/qsize)*bootsize*sizeof(double),64);
+      for(int q=0; q<qsize; q++)
+	{
+	  cout<<"q iteration "<<q<<endl;
+	  for(int i=0; i<(rowsize/qsize);i++)
+	    {
+	      for(int j=0;j<colsize;j++)
+		{
+		  cblas_dcopy(bootsize,&matrix[index((q*(rowsize/qsize))+i,j,colsize)],colsize*rowsize,&tmatrix[index(i,j,colsize)],colsize*(rowsize/qsize));
+		}
+	    }
+
+	  
+	  status = vsldSSNewTask( &task,&p,&n,&x_storage,(double*)tmatrix,NULL,NULL);
+	  status = vsldSSEditQuantiles(task,&deciles,o_quant,(double*)&quantiles[multindex(q*(rowsize/qsize),0,0,rowsize,colsize,2)],(double*)o_stat,&o_storage);
+	  status = vsldSSCompute(task,VSL_SS_QUANTS,VSL_SS_METHOD_FAST);
+	  status = vslSSDeleteTask(&task);
+	  
+	}
+      mkl_free(o_stat);
+      mkl_free(tmatrix);
+    }
+  else{
+
+    status = vsldSSNewTask( &task,&p,&n,&x_storage,(double*)matrix,NULL,NULL);
 
 
-  status = vsldSSEditQuantiles(task,&deciles,o_quant,(double*)quantiles,(double*)o_stat,&o_storage);
+    status = vsldSSEditQuantiles(task,&deciles,o_quant,(double*)quantiles,(double*)o_stat,&o_storage);
 
-  status = vsldSSCompute(task,VSL_SS_QUANTS,VSL_SS_METHOD_FAST);
+    status = vsldSSCompute(task,VSL_SS_QUANTS,VSL_SS_METHOD_FAST);
 
-  status = vslSSDeleteTask(&task);
-
+    status = vslSSDeleteTask(&task);
+  }
 
 
 
   
 }
 
-double* TestGenerate(double *&matrix, int rowsize,int colsize,int rowstart, int colstart,int zindex)
+void TestGenerate(double *matrix, size_t rowsize,size_t colsize,size_t rowstart, size_t colstart,size_t zindex)
 //Create a 2d test array for the rest of the functions
 { 
   for(int i=0; i<rowsize;i++)
@@ -392,129 +224,54 @@ double* TestGenerate(double *&matrix, int rowsize,int colsize,int rowstart, int 
 	  matrix[index(i,j,colsize)]=((double) rowstart+(double)i)+((double) colstart+(double)j*0.1)+(double)zindex*0.01;
 	}
     }
-  return(matrix);
+
 }
 
-bool isCis(const int* snparray,const int* genearray,int snp,int gene,int cisdist)
+double quantile(double *sarray, double quantile, size_t size,size_t skipsize)
 {
-  int snpchr = snparray[index(snp,0,2)];
-  int snppos = snparray[index(snp,1,2)];
-  int genechr = genearray[index(gene,0,3)];
-  int genestart = genearray[index(gene,1,3)];
-  int geneend = genearray[index(gene,2,3)];
-  
-  return( (snpchr==genechr)&&((abs(snppos-genestart)<cisdist)||(abs(snppos-geneend)<cisdist)));
-  
-}
-void CisTransOut (const double *quantilearray,const int snpstart,const int snpsize,const int genestart,const int genesize, const int casesize,const int zsize,const char *annofile,double t_thresh,const char*cisfile,const char* transfile,int cisdist,const char* lockfile,int mychunk,const char* progfile)
-  
-{
+  double pos=quantile*size;
 
-  struct stat buffer;
-  int status;
-  FILE* fp;
-  ofstream cis,trans,prog;
-  double cor1,cor2,worstcor;
-  double cordenom;
-  int *snparray,*genearray;
-  char** snpnames,**genenames;
 
-  while(stat(lockfile,&buffer)==0)
+  if(ceilf(pos)==pos)
     {
-      usleep(9999);
-      cout<<lockfile<<" exists!"<<endl;
+      return(sarray[(int)(pos)*skipsize]);
     }
-  fp = fopen(lockfile,"ab+");
-
-  ReadAnno(snpnames,snparray,snpstart,snpsize,annofile,2);
-  ReadAnno(genenames,genearray,genestart,genesize,annofile,3);
-  
-  cordenom = sqrt(casesize-2);
-
-
-  prog.open(progfile,ofstream::out|ofstream::app);
-  prog<<mychunk<<endl;
-  cis.open(cisfile,ofstream::out|ofstream::app);
-  trans.open(transfile,ofstream::out|ofstream::app);
-  if(mychunk==0){
-    cis<<"SNP\tGene\tt-stat1\tt-stat2"<<endl;
-    trans<<"SNP\tGene\tt-stat1\tt-stat2"<<endl;
-  }
-  
-  for(int i=0; i<snpsize; i++)
+  else
     {
-      for(int j=0; j<genesize; j++)
-	{
-	  cor1=quantilearray[multindex(i,j,0,snpsize,genesize,zsize)];
-	  cor1=cordenom*(cor1/sqrt(1-(cor1*cor1)));
-	  cor2=quantilearray[multindex(i,j,1,snpsize,genesize,zsize)];
-	  cor2=cordenom*(cor2/sqrt(1-(cor2*cor2)));
-	  worstcor = fabs(cor1)>fabs(cor2) ? cor2 : cor1;
-	  //	  cout<<snpnames[i]<<"-"<<genenames[j]<<": "<<fabs(worstcor)<<" "<<t_thresh<<endl;
-	  
+      double fpos = sarray[(int)floor(pos)*skipsize];
+      double cpos = sarray[(int)ceil(pos)*skipsize];
 
-	  if(fabs(worstcor)>t_thresh && ((cor1<0)==(cor2<0))&&((!isnan(cor1))&&(!isnan(cor2))))
-	    {
-	      if(isCis(snparray,genearray,i,j,cisdist))
-		{
-		  cis<<snpnames[i]<<"\t"<<genenames[j]<<"\t"<<cor1<<"\t"<<cor2<<endl;
-		}
-	      else
-		{
-		  trans<<snpnames[i]<<"\t"<<genenames[j]<<"\t"<<cor1<<"\t"<<cor2<<endl;
-		}
-	    }
-	}
-    } 
-
-  prog.close();
-  trans.close();
-  cis.close();
-  fclose(fp);
-  remove(lockfile);
-
+      return(fpos+((cpos-fpos))*(pos-floor(pos)));
+    }
 }
 
-
-
-
-
-
-void readparam(hid_t file_id, const char* attributename,char*&attributevalue)
+void quantile2 (double *&matrix, size_t rowsize, size_t colsize, size_t bootsize, double *&quantiles)
 {
-  hid_t root,att;
-  hid_t fstrtype;
-
-  root=H5Gopen(file_id,"/",H5P_DEFAULT);
-  fstrtype= H5Tcopy(H5T_C_S1);
-  H5Tset_size(fstrtype,H5T_VARIABLE);
-  att=H5Aopen_name(root,attributename);
-  H5Aread(att,fstrtype,&attributevalue);
-  H5Gclose(root);
-  H5Aclose(att);
-}
-	  
-void readparam(hid_t file_id,const char* attributename,int&attributevalue)
-{
-  hid_t root,att;
-  hid_t fstrtype;
+  size_t skipsize = rowsize*colsize;
   
-  root = H5Gopen(file_id,"/",H5P_DEFAULT);
-  att=H5Aopen_name(root,attributename);
-  H5Aread(att,H5T_NATIVE_INT,&attributevalue);
-  H5Gclose(root);
-  H5Aclose(att);
+  for(size_t i=0; i<skipsize;i++)
+    {
+      unsigned percent=(((double) i)/((double) skipsize))*100;
+      printProgBar(percent);
+      stride_iter<double*> Cstart(matrix+i,skipsize);
+      stride_iter<double*> Cend(matrix+(i+(skipsize*(bootsize-1))),skipsize);
+      sort(Cstart,Cend);
+      quantiles[i*3]=quantile(matrix+i,0.025,bootsize,skipsize);
+      quantiles[(i*3)+1]=quantile(matrix+i,0.5,bootsize,skipsize);
+      quantiles[(i*3)+2]=quantile(matrix+i,0.975,bootsize,skipsize);
+    }
 }
-
-void readparam(hid_t file_id,const char* attributename,double&attributevalue)
-{
-  hid_t root,att;
-  hid_t fstrtype;
   
-  root = H5Gopen(file_id,"/",H5P_DEFAULT);
-  att=H5Aopen_name(root,attributename);
-  H5Aread(att,H5T_NATIVE_DOUBLE,&attributevalue);
-  H5Gclose(root);
-  H5Aclose(att);
-}
+  
+
+
+
+
+  
+
+
+
+
+
+
 #endif
